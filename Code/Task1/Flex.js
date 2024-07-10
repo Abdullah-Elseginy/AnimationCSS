76abf9aae83047b9e3a4ac3d431ca4b9f7bc1b2d
Abdullah Elseginy <abdullah.ahmed.elseginy@gmail.com>
2024-07-11 00:48:06 +0300

    MyAnimation

---
+++ Code/Task1/Flex.js
@@ -0,0 +1,30 @@
+// var arr = [1, 2, 3, 4, 5, 6];
+// var data = [
+//   {
+//     title: "Fresh and clean",
+//     iconClass: "fa fa-bullhorn",
+//     paragraph: "lorem lorem lorem lorem loreml orem lorem lorem loreml",
+//   },
+//   {
+//     title: "no normando lala",
+//     iconClass: "fa fa-bullhorn",
+//     paragraph: "lorem lorem lorem lorem loreml orem lorem lorem loreml",
+//   },
+//   {
+//     title: "sui la vie",
+//     iconClass: "fa fa-bullhorn",
+//     paragraph: "lorem lorem lorem lorem loreml orem lorem lorem loreml",
+//   },
+// ];
+
+// var cer = document.querySelector(".cer");
+// var titl = document.querySelector(".title");
+// var para = document.querySelector(".para");
+// var contaner = document.querySelector(".container");
+// var maincontainer = document.querySelector(".maincontainer");
+// data.map((ele, index) => {
+//   cer.class = ele.iconClass;
+//   titl.innerText = ele.title;
+//   para.innerText = ele.paragraph;
+//   maincontainer.append(contaner);
+// });
